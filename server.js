8028dd78aa4c54718dd734c8a37c9dc385faad76
Varun <gdevvarunsha@gmail.com>
2026-04-14 22:17:42 +0530

    final fix

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("App is woring 🚀");
+  res.send("App isc woring 🚀");
 });
 
 app.listen(3000, "0.0.0.0", () => {
