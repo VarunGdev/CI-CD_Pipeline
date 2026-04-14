47cd633f153a55044e254522bead6ccdb6d18ba7
Varun <gdevvarunsha@gmail.com>
2026-04-14 21:52:14 +0530

    s

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("App is working 🚀");
+  res.send("App is woring 🚀");
 });
 
 app.listen(3000, "0.0.0.0", () => {
